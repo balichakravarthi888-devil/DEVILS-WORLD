728a2982785dfea4c3b80fe3ff678e7e3cf6966b
balichakravarthi888-devil <balichakravarthi888@gmail.com>
2025-10-30 09:57:35 +0530

    Fix typo in third line print statement

--- 03_PRG3.C
+++ 03_PRG3.C
@@ -6,7 +6,8 @@ void main()
 clrscr();
 printf("\n this is first line\t in my c programe\b\a");
 printf("\n this is second line in my c programe");
-printf("\n this i third line in my c programe");
+printf("\n this is third line in my c programe");
 printf("\n this is fourth line in my c programe");
 getch();
+
 }
